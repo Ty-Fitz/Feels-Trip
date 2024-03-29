229f1ec1f5d1c737e4f8f1cbc27d13d500ad69d7
Tyler Fitzgerald <127678239+Ty-Fitz@users.noreply.github.com>
2024-03-29 17:06:26 -0400

    uploaded working and compiling vial base

---
+++ feelstrip/keymaps/vial/config.h
@@ -0,0 +1,24 @@
+/* Copyright 2020 Matt3o
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x27, 0x5D, 0xA8, 0x05, 0xC8, 0x62, 0x31}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+
+// place overrides here
